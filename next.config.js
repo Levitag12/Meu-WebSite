81e2babe251a1d59f43467e93efdac1aa87d7fce
Welington Lima <44483236-levitaajogos2@users.noreply.replit.com>
2025-07-01 18:21:17 +0000

    correção

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["@node-rs/argon2"]
+  serverExternalPackages: ["@node-rs/argon2"],
+  experimental: {
+    serverActions: true
+  },
+  allowedDevOrigins: [
+    "http://localhost:3001",
+    "https://01c6def6-6da5-4ea1-9950-dc3169c4eeb5-00-30iqj27xr6025.janeway.replit.dev/login?callbackUrl=%2Fdashboard"
+  ]
 }
 
 module.exports = nextConfig
